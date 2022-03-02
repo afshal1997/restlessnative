c530c440c7cb3993adaca9700edd8605177f8f56
Afshal Warsi <afshal.warsi@shottech.co>
2022-03-02 12:32:22 -0800

    updates

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import HomeSection02 from "../../Layout/HomePageLayout/HomeSection02";
 import MainSection from "../../Layout/HomePageLayout/MainSection";
 export default function Home() {
     return (
         <div>
             <MainSection />
+            <HomeSection02/>
         </div>
     )
 }
---
+++ src/Layout/HomePageLayout/HomeSection02.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeSection02 = () => {
+  return (
+    <div>HomeSection02</div>
+  )
+}
+
+export default HomeSection02
