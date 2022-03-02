873f1137c7c18f9ee15b8ed74416197c9af628f7
Afshal Warsi <afshal.warsi@shottech.co>
2022-03-02 12:22:43 -0800

    updates

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,11 +1,9 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import MainSection from "../../Layout/HomePageLayout/MainSection";
 export default function Home() {
     return (
         <div>
             <MainSection />
-
         </div>
     )
 }
--- src/Layout/HomePageLayout/MainSection.js
+++ src/Layout/HomePageLayout/MainSection.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, Col, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import './home.css'
 
 function MainSection() {
     return (
